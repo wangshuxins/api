7ed52beddd426444a65b7c56ccf1e3c29c53db60
wangshuxin <1357252390@qq.com>
2020-07-15 03:36:57 +0000

    获取

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test(){
+        $appid = "wx3d6c4ee4ee8c6159";
+        $secret = "07a2621b89e3d7aad365687deba1118e";
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$secret;
+        $content = file_get_contents($url);
+        echo $content;
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hello Laravel
+                   柯南
                 </div>
 
                 <div class="links">
